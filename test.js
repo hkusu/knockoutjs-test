99c23b9eadd34e42b21d852ab6e9cb580918f329
hkusu <kusu0806@gmail.com>
2014-02-03 13:53:39 +0900

    test

--- test.js
+++ test.js
@@ -1,18 +1,15 @@
-$(function() {
+function AppViewModel() {
+	this.firstName = ko.observable("Bert");
+	this.lastName = ko.observable("Bertington");
 
-	function AppViewModel() {
-		this.firstName = ko.observable("Bert");
-		this.lastName = ko.observable("Bertington");
+	this.fullName = ko.computed(function() {
+		return this.firstName() + " " + this.lastName();
+	}, this);
 
-		this.fullName = ko.computed(function() {
-			return this.firstName() + " " + this.lastName();
-		}, this);
+	this.capitalizeLastName = function() {
+		var currentVal = this.lastName();
+		this.lastName(currentVal.toUpperCase());
+	};
+}
 
-		this.capitalizeLastName = function() {
-			var currentVal = this.lastName();
-			this.lastName(currentVal.toUpperCase());
-		};
-	}
-
-	ko.applyBindings(new AppViewModel());
-});
+ko.applyBindings(new AppViewModel());
